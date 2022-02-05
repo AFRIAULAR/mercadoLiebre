7da6aeb802b25a017f052f593ab58fad06ecb5d0
AFRIAULAR <aularafrica@gmail.com>
2022-02-04 22:55:54 -0300

    Agregando rutas

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+app.listen(8000, ()=> console.log("Servidor corriendo"));
+
 const path = require('path');
-app.listen(8000);
-const publicPath = path.resolve(__dirname, 'public');
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
-app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
